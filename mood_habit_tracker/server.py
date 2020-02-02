91a9b45e03f013d0b1b53afadca7589aa6ee71a8
Kris <KrisCaseyBiotech@gmail.com>
2020-02-02 01:31:27 +0000

    install psycopg2 and update requirements.txt

--- mood_habit_tracker/server.py
+++ mood_habit_tracker/server.py
@@ -1,16 +1,18 @@
-from flask import Flask, jsonify, render_template
-from model import connect_to_db, User, Habit, Mood, Weather
+from flask import (Flask, jsonify, render_template)
+from model import (connect_to_db, User, Habit, Mood, Weather)
 
 app = Flask(__name__) #What? Why not turning red?
 app.secret_key = 'secrets are fun'
 
-@app.route('/')
-def homepage:
-    """Show homepage."""
+# @app.route('/')
+# def homepage():
+#     """Show homepage."""
 
-    return render_template("index.html")
+#     return render_template('index.html')
+
+# @app.route('/moods')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     connect_to_db(app)
-    app.run(host="0.0.0.0", debug=True)
+    app.run(port=5000, host='0.0.0.0', debug=True)
