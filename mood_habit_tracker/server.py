0c0112e6c681f59d00f867e02965e44e96eaef5f
Kris <KrisCaseyBiotech@gmail.com>
2020-02-08 20:40:51 +0000

    Add x zipcode entry for habit and mood forms

--- mood_habit_tracker/server.py
+++ mood_habit_tracker/server.py
@@ -11,11 +11,13 @@ PLACEHOLDER = 1
 PLACEHOLDER_ZIP = '94404,us'
 PLACEHOLDER_DATE = '2010-01-02 01:00:00'
 
+
 @app.route('/')
 def get_homepage():
     """Show homepage."""
 
     return 'Homepage'
+
 
 @app.route('/moods', methods=['GET'])
 def get_mood():
@@ -25,11 +27,13 @@ def get_mood():
 
     return render_template("input_mood.html", moods=moods)
 
+
 @app.route('/moods', methods=['POST'])
 def post_mood():
     """Post mood data."""
 
-    weather_id = get_weather()
+    zipcode = request.form.get('zipcode')
+    weather_id = get_weather(zipcode)
 
     mood = request.form.get('mood_options')
     intensity = request.form.get('intensity')
@@ -43,6 +47,7 @@ def post_mood():
 
     return render_template("mood_entered.html")
 
+
 @app.route('/habits', methods=['GET'])
 def get_habit():
     """Create habit form."""
@@ -51,27 +56,31 @@ def get_habit():
 
     return render_template("input_habit.html", habits=habits)
 
+
 @app.route('/habits', methods=['POST'])
 def post_habit():
     """Post habit data."""
 
-    weather_id = get_weather()
+    zipcode = request.form.get('zipcode')
+    weather_id = get_weather(zipcode)
 
     habit = request.form.get('habit_options')
     ins = Habit(habit=habit, 
                user_id=PLACEHOLDER, 
-               weather_id=weather_id)#PLACEHOLDER
+               weather_id=weather_id)
 
     db.session.add(ins)    
     db.session.commit()
 
     return render_template("habit_entered.html")
 
-def get_weather():
+
+
+def get_weather(zipcode):
 
     url = 'http://api.openweathermap.org/data/2.5/weather'
     payload = {'APPID' : key,
-               'zip' : PLACEHOLDER_ZIP }
+               'zip' : f'{zipcode},us'}
 
     # Get the current weather response from the openWeather API
     res = requests.get(url, params=payload)
@@ -99,6 +108,7 @@ def get_weather():
                                          Weather.time == PLACEHOLDER_DATE).all()
 
     return weather_entry[0].weather_id
+
 
 
 if __name__ == '__main__':
