c44438669d81fc259d51e96b8a2dc2b8e755dff4
Kris <KrisCaseyBiotech@gmail.com>
2020-03-13 00:38:06 +0000

    fix a centering error

--- mood_habit_tracker/static/js/homepage.jsx
+++ mood_habit_tracker/static/js/homepage.jsx
@@ -2,7 +2,7 @@ class Homepage extends React.Component {
     render() {
         return(
             
-            <div id="homepage" className="d-flex align-self-center min-vh-100">
+            <div id="homepage" className="d-flex align-self-center justify-content-center min-vh-100">
                 <h1>Welcome to the Mood & Habit Tracker</h1>
             </div>
             
